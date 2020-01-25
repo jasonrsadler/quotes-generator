313ecc021574b805909517ddbb7d3b6b011cc07e
Jason Sadler <jasonsadler@amplifiedit.com>
2020-01-25 14:15:13 -0500

    master version

---
+++ src/src/Quotebox.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import quotes from './quotes/quotes'
+
+class Quotebox extends Component {
+  constructor(props) {
+    super(props)
+    const index = Math.floor(Math.random() * Math.floor(quotes.length))
+    this.state = {
+      randomQuote: quotes[index].quote,
+      author: quotes[index].author
+    }
+  }
+
+  getNewQuote = () => {
+    const index = Math.floor(Math.random() * Math.floor(quotes.length))
+    this.setState({
+      randomQuote: quotes[index].quote,
+      author: quotes[index].author
+    })
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <span id="author">
+          Author: {this.state.author}
+        </span>
+        <div id="text">
+          Quotebox: {this.state.randomQuote}
+        <a href={`${'https://twitter.com/intent/tweet?' + this.state.randomQuote}`} id="tweet-quote">
+          Reteet
+        </a>
+        </div>
+        <div>
+          <button id="new-quote" onClick={this.getNewQuote}>Get New</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Quotebox
